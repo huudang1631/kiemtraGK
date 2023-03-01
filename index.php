45e6ffc778b2e3163d24bcd791b59700832764c4
= <=>
2023-03-01 11:02:00 +0700

    tho commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2>Đăng Nhập</h2>
+    <form action="" method="">
+        <label for="">Tên Người Dùng</label>
+        <input type="text" name="username" class="txtUserName">
+    </form>
+</body>
+
+</html>
